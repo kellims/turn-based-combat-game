d091100a2516a01acb3033f373ebbe39937a18eb
kellims <kellisimon18@gmail.com>
2023-04-26 19:03:24 -0600

    updated with basics of state, props, and mapping

--- src/components/Choices.js
+++ src/components/Choices.js
@@ -1,12 +1,16 @@
 import { useState } from "react";
 
 function Choices({choice}) {
-    const [health, setHealth] = useState(props.choice.health)
-    
+    const [health, setHealth] = useState(choice.health)
+    function increaseHealth() {
+        setHealth(health + 5);
+    }
     return (
        <>
             <h2>Name: {choice.name}</h2>
-            <h3>Health: {choice.health}</h3>
+            <div className="health">
+            <h3>Health: {health}</h3><button onClick={increaseHealth}>+</button>
+            </div>
             <h3>Power: {choice.power}</h3>
             <h3>Accuracy: {choice.accuracy}</h3>
             <hr></hr>
